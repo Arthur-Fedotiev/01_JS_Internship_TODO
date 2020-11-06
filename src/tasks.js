265885c459061b7a19fb0eddbf4d171a01b9572e
Arthur Fedotiev <artur.fedotiew@gmail.com>
2020-11-06 17:33:03 +0200

    US001: Created TODO

---
+++ src/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+  {
+    id: 1,
+    content: "Wake up",
+    completed: true,
+  },
+  {
+    id: 2,
+    content: "Have some coffee",
+    completed: false,
+  },
+  {
+    id: 3,
+    content: "Learn Angular",
+    completed: false,
+  },
+];
+
+export default tasks;
