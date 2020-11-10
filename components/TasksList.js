8b8d4a9ce8ef632fb15226cb780eebbe8981731b
Arthur Fedotiev <artur.fedotiew@gmail.com>
2020-11-10 16:34:18 +0200

    US-006: Hide/show item functionality added

---
+++ components/TasksList.js
@@ -0,0 +1,40 @@
+export default class TasksList {
+  constructor(container) {
+    this.container = container;
+  }
+
+  render(tasks, showTasks) {
+    console.log(showTasks);
+    const tasksToShow =
+      showTasks === "all"
+        ? tasks
+        : tasks.filter((t) => {
+            return showTasks === "completed" ? t.completed : !t.completed;
+          });
+    this.container.innerHTML = `<div class="form-check">${tasksToShow
+      .map(
+        (t) => `<div class="row">
+          <div class="col-3">
+          <input class="form-check-input my-3" type="checkbox" name="task" id=${
+            t.id
+          } ${t.completed ? "checked" : ""} ">
+          
+        <label class="form-check-label my-2 ${
+          t.completed && "text-muted"
+        }" for=${t.id}> ${
+          t.completed ? `<del>${t.content}</del>` : t.content
+        }</label>
+          <i class="fas fa-pencil-alt" title="Edit" data-role="editTask" id=${
+            t.id
+          }></i><br>
+          </div>
+          <div class="col-2">
+          <input type="button" id=${
+            t.id
+          } name="deleteBtn" value="-"} class="btn btn-secondary"></input>
+          </div>
+          </div>`
+      )
+      .join("")}</div>`;
+  }
+}
